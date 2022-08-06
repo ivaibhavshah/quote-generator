68b7beba8f30108c874490c903d2041a29254497
Vaibhav Abhaybhai Shah <vaibhav.anjar@gmail.com>
2022-08-06 18:08:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict';
+// Loading all selectors
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote-text');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// complete loading /hide loader
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden =false;
+}
+// loading function
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+    
+}
+let apiQuotes = []; 
+
+
+// Get quotes from api
+
+// https://quotes-react.netlify.app/
+// Show new quote
+function newQuote() {
+    loading();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    console.log(quote);
+    quoteText.textContent = quote.text;
+    if(!quote.author){
+        authorText.textContent = "Unknown";
+    }
+    else{
+        authorText.textContent = quote.author;
+    }
+    if(quote.text.length>120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    complete();
+}
+
+async function getQuotes() {
+    const apiURL ="https://type.fit/api/quotes/";
+    try {
+        loading();
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
+        complete();
+    }
+    catch (error){
+        //catch error
+        alert("Please load Page again");
+    } 
+}
+
+// Whatsapp button
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+
+// Onload
+getQuotes();
+
